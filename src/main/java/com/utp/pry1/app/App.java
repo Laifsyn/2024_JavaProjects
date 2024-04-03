561b85011c3b9d28ff8d30fd0184d66bad6f98cd
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2024-04-03 19:14:04 +0000

    Pull Utils Class into public

--- src/main/java/com/utp/pry1/app/App.java
+++ src/main/java/com/utp/pry1/app/App.java
@@ -3,7 +3,6 @@ package com.utp.pry1.app;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class App {
     void spawn_cli() {
@@ -141,32 +140,4 @@ class Int2Words {
             "setecientos", "ochocientos", "novecientos"};
     static final String[] MILES = {"", "mil", "millones", "billones"};
     static final String[] MIL = {"", "mil", "millón", "billón"};
-}
-
-class Utils {
-    static public Integer[] into_triplets(Integer num) {
-
-        var vec = new ArrayList<Integer>();
-        while (num > 0) {
-            vec.add(num % 1000);
-            num /= 1000;
-        }
-        Collections.reverse(vec);
-        // System.out.println(vec.toString());
-        return vec.toArray(new Integer[0]);
-    }
-
-    static public void pretty_print_int(Integer num) {
-        var triplets = Utils.into_triplets(num);
-        for (int i = 0; i < triplets.length; i++) {
-            if (i == 0) {
-                System.out.print(triplets[i]);
-            } else {
-                System.out.printf("%03d", triplets[i]);
-            }
-            if (i < triplets.length - 1) {
-                System.out.print(",");
-            }
-        }
-    }
 }
---
+++ src/main/java/com/utp/pry2/app/RepNumericas.java
@@ -0,0 +1,14 @@
+package com.utp.pry2.app;
+
+public class RepNumericas {
+    static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("RepNumericas");
+    }
+}
+
+class Utils {
+    static void pretty_print_int(int num) {
+        com.utp.pry1.app.Utils.pretty_print_int(num);
+    }
+}
