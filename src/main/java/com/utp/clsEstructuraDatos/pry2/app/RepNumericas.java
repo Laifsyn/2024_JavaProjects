8d2f224c0441cbc33190f4a714375dfd61254b97
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2024-04-10 21:51:09 -0500

    Representaciones Numericas

--- src/main/java/com/utp/clsEstructuraDatos/pry2/app/RepNumericas.java
+++ src/main/java/com/utp/clsEstructuraDatos/pry2/app/RepNumericas.java
@@ -1,14 +1,80 @@
 package com.utp.clsEstructuraDatos.pry2.app;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.util.Optional;
 
 import com.utp.utils.Result;
 
 public class RepNumericas {
+    static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    static String buffer = null;
+
+    static String ReadLine() {
+        try {
+            buffer = reader.readLine().trim();
+        } catch (Exception e) {
+            System.out.println("Entrada invalida: " + e);
+        }
+
+        if (buffer.equals("exit")) {
+            System.out.println("Saliendo...");
+            System.exit(0);
+        }
+
+        return buffer;
+    }
 
     public static void main(String[] args) {
-        BufferedRead
+        String entrada = null;
+        System.out.println("Bienvenido al programa de representación numérica. Escriba `exit` para salir cuando sea.");
+        while (true) {
+
+            System.out.println("Elija la representación numérica (binario, entero, hexadecimal): ");
+            // El programa debe aceptar un número ingresado por el usuario, indicando si es
+            // binario, decimal o hexadecimal.
+            entrada = ReadLine();
+            Optional<RepNumerica> maybe_rep = RepNumerica.from_str(entrada);
+            if (maybe_rep.isEmpty()) {
+                System.out.printf("`%s` No es una representación válida.\n", entrada);
+                continue;
+            }
+            // Guardamos la representación numérica
+            var representacion = maybe_rep.get();
+            System.out.printf("Elegiste la representación `%s`\n", representacion);
+            System.out.println("Ingrese el número a representar: ");
+            // Leemos el número a representar en la representación seleccionada
+            entrada = ReadLine();
+            Result<BigInteger, String> maybe_BigInt = RepresentacionNumerica.check_string(representacion, entrada);
+            if (maybe_BigInt.isError()) {
+                System.out.println(maybe_BigInt.unwrapError());
+                continue;
+            }
+            // Guardamos el número.
+            var bigInt = maybe_BigInt.unwrapOk();
+            // Creamos una instancia de RepresentacionNumerica que puede ser convertida a
+            // cualquier otra representación soportada
+            RepresentacionNumerica rep = new RepresentacionNumerica(bigInt, representacion);
+            System.out.println("El número en la representación seleccionada es: " + rep.to_string());
+
+            // Mostrar en pantalla los equivalentes del número ingresado en los sistemas
+            // numéricos restantes, según corresponda
+            switch (representacion) {
+                case BINARIO -> {
+                    System.out.println("El número en entero es: " + rep.as_string_in(RepNumerica.ENTERO));
+                    System.out.println("El número en hexadecimal es: " + rep.as_string_in(RepNumerica.HEXADECIMAL));
+                }
+                case ENTERO -> {
+                    System.out.println("El número en binario es: " + rep.as_string_in(RepNumerica.BINARIO));
+                    System.out.println("El número en hexadecimal es: " + rep.as_string_in(RepNumerica.HEXADECIMAL));
+                }
+                case HEXADECIMAL -> {
+                    System.out.println("El número en binario es: " + rep.as_string_in(RepNumerica.BINARIO));
+                    System.out.println("El número en entero es: " + rep.as_string_in(RepNumerica.ENTERO));
+                }
+            }
+        }
     }
 }
 
@@ -58,10 +124,15 @@ record RepresentacionNumerica(BigInteger innerBigInt, RepNumerica type) {
     }
 
     public String to_string() {
+        return as_string_in(type);
+    }
+
+    public String as_string_in(RepNumerica type) {
+        // Convertir el número ingresado a sus equivalentes en un sistema numérico.
         return switch (type) {
-            case BINARIO -> innerBigInt.toString(2);
+            case BINARIO -> "0b" + innerBigInt.toString(2);
             case ENTERO -> innerBigInt.toString(10);
-            case HEXADECIMAL -> innerBigInt.toString(16);
+            case HEXADECIMAL -> "0x" + innerBigInt.toString(16).toUpperCase();
         };
     }
 
