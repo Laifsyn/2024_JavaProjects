a7fe7ea3d9d6827e8ef5ed04d158fa2c62261730
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2024-04-12 02:34:41 -0500

    Pry2 de Herramientas

---
+++ src/main/java/com/utp/clsHerramientas/pry2/App.java
@@ -0,0 +1,29 @@
+package com.utp.clsHerramientas.pry2;
+
+public class App {
+
+    // Escriba un programa en java que permita poner en funcionamiento los
+    // siguientes módulos:
+    // 
+    // • Diseñar un módulo para calcular la velocidad (en m/s) de los corredores de
+    // la carrera de 1,500 metros. La entrada consistirá en parejas de números
+    // (minutos, segundos) que dan el tiempo del corredor; por cada corredor, el
+    // algoritmo debe visualizar el tiempo en minutos y segundos, así como la
+    // velocidad media. Ejemplo de entrada de datos: (3,53) (3,40) (3,46) (3,52)
+    // (4,0) (0,0); el último par de datos se utilizará como fin de entrada de
+    // datos.
+    //
+    // • Muchos bancos y cajas de ahorro calculan los intereses de las cantidades
+    // depositadas por los clientes diariamente según las premisas siguientes. Un
+    // capital de 1,000 dólares, con una tasa de interés del 6 por 100, renta un
+    // interés en un día de 0.06 multiplicado por 1,000 y dividido por 365. Esta
+    // operación producirá 0,16 dólares de interés y el capital acumulado será
+    // 1,000.16. El interés para el segundo día se calculará multiplicando 0.06 por
+    // 1,000 y dividiendo el resultado por 365. Diseñar un módulo que reciba tres
+    // entradas: el capital a depositar, la tasa de interés y la duración del
+    // depósito en semanas, y calcular el capital total acumulado al final del
+    // período de tiempo especificado.
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
