d2304bf820914dcc850e8f9449b516803a18cecf
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2024-05-11 20:52:06 +0000

    Implementar el Dibujador de Tabla

--- src/main/java/com/utp/clsEstructuraDatos/pry3/DrawTable.java
+++ src/main/java/com/utp/clsEstructuraDatos/pry3/DrawTable.java
@@ -3,18 +3,19 @@ package com.utp.clsEstructuraDatos.pry3;
 import java.util.ArrayList;
 
 public class DrawTable {
-    private int columnas = 0;
-    private ArrayList<String[]> table = null;
+    private int cantidad_columnas = 0;
+    private ArrayList<String[]> table;
 
     public DrawTable() {
+        this.table = new ArrayList<>();
     }
 
     public void insertar_fila(String[] row) {
-        if (this.columnas == 0) {
-            this.columnas = row.length;
+        if (this.cantidad_columnas == 0) {
+            this.cantidad_columnas = row.length;
         }
         // Todas las filas tienen que tener la misma cantidad columna
-        if (this.columnas != row.length) {
+        if (this.cantidad_columnas != row.length) {
             throw new IllegalArgumentException("La fila no tiene la misma cantidad de columnas que las anteriores");
         }
         this.table.add(row);
@@ -22,61 +23,83 @@ public class DrawTable {
 
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        throw new UnsupportedOperationException("asdasdsa");
-        // for (String[] row : this.table) {
-        // for (String cell : row) {
-        // sb.append(cell);
-        // sb.append(" ");
-        // }
-        // sb.append("\n");
-        // }
-        // return sb.toString();
+        // Iniciamos en 1 para acontar el borde izquierdo de la tabla
+        int max_width = 1;
+        // Guardo la posicion de los vertices de la tabla.
+        Integer[] columns_widths = new Integer[this.cantidad_columnas];
+
+        // Obtener espacio total para dibujo antes de empezar a dibujar la tabla, y
+        // tambien obtener los vertices de la tabla
+
+        for (int indice_columna = 0; indice_columna < this.cantidad_columnas; indice_columna++) {
+            int ancho_columna = 0;
+            for (String[] fila : this.table) {
+                String celda = fila[indice_columna];
+                if (celda.length() > ancho_columna) {
+                    // +1 para el caracter que encierra la celda
+                    ancho_columna = celda.length() + 1;
+                    columns_widths[indice_columna] = ancho_columna;
+                }
+            }
+            max_width += ancho_columna;
+        }
+
+        StringBuilder borde_superior = new StringBuilder("┌");
+        StringBuilder separador_fila = new StringBuilder("├");
+        StringBuilder borde_inferior = new StringBuilder("└");
+        // Construimos los separadores
+        for (int indice_columna = 0; indice_columna < this.cantidad_columnas; indice_columna++) {
+            int padding = columns_widths[indice_columna];
+            // El ultimo caracter se encierra con algo distinto.
+
+            borde_superior.append(String.format("%" + padding + "s", "┬").replace(" ", "─"));
+            separador_fila.append(String.format("%" + padding + "s", "┼").replace(" ", "─"));
+            borde_inferior.append(String.format("%" + padding + "s", "┴").replace(" ", "─"));
+            if (indice_columna == this.cantidad_columnas - 1) {
+                borde_superior.setLength(borde_superior.length() - 1);
+                separador_fila.setLength(separador_fila.length() - 1);
+                borde_inferior.setLength(borde_inferior.length() - 1);
+            }
+        }
+        borde_superior.append("┐\n");
+        separador_fila.append("┤\n");
+        borde_inferior.append("┘\n");
+
+        sb.append(borde_superior);
+        char barra_vertical = '│';
+        // Usamos Vanilla For para poder insertar separador de fila despues del
+        // encabezado
+        for (int indice_fila = 0; indice_fila < this.table.size(); indice_fila++) {
+            sb.append(barra_vertical);
+            for (int indice_columna = 0; indice_columna < this.cantidad_columnas; indice_columna++) {
+                String celda = this.table.get(indice_fila)[indice_columna];
+                int padding = columns_widths[indice_columna];
+                sb.append(String.format("%" + padding + "s", celda + barra_vertical));
+            }
+            sb.append("\n");
+            if (indice_fila == 0) {
+                sb.append(separador_fila);
+            }
+        }
+        sb.append(borde_inferior);
+        return sb.toString();
     }
 
     /*
      * |__|| asdfa||c|
      * ------------------------
      * | 1||23jgadsdsavghgh||3|
-     * | 4||              5||6|
+     * | 4|| 5||6|
      */
     public static void main(String[] args) {
 
-        String[][] mi_tabla = new String[][] { { "____", "asdfa", "c" }, { "1", "23jgadsdsavghgh", "3" },
+        String[][] mi_tabla = new String[][] { { "Hola mundo", "asdfa", "Nostradamus" },
+                { "1", "23jgadsdsavghgh", "3" },
                 { "4", "5", "6" } };
-        ArrayList<Integer> ancho_columna = new ArrayList<>();
-        System.err.println("Antes  : " + ancho_columna.toString());
-        for (String celda : mi_tabla[0]) {
-            ancho_columna.add(celda.length());
-        }
-
-        System.out.println("Primera vuelta te va a dar: " + ancho_columna.toString());
-        for (String[] fila : mi_tabla) {
-            for (int i = 0; i < fila.length; i++) {
-                if (ancho_columna.get(i) < fila[i].length()) {
-                    ancho_columna.set(i, fila[i].length());
-                }
-            }
-        }
-
-        System.out.println();
-        System.err.println("Despues: " + ancho_columna.toString());
-
-        System.out.println("Tabla de verdad");
-        for (int j = 0; j < mi_tabla.length; j++) {
-            for (int i = 0; i < mi_tabla[j].length; i++) {
-                // Por defecto
-                System.out.print(String.format("|%" + ancho_columna.get(i) + "s|", mi_tabla[j][i]));
-            }
-            System.out.println();
-           
-            if (j == 0) {
-                for (int k = 0; k < mi_tabla[0].length; k++) {
-                    System.out.print("|" + "=".repeat(ancho_columna.get(k)) + "|");
-                }
-                System.out.println();
-            }
-        }
+        DrawTable table = new DrawTable();
+        for (String[] fila : mi_tabla)
+            table.insertar_fila(fila);
+        System.out.println(table.toString());
         System.exit(0);
     }
 }
-    
