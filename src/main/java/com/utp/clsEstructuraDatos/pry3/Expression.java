964a3e4a0953dfa64138881afc2e8b66fefc75d1
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2024-05-13 22:19:58 +0000

    Invert variable population order

--- src/main/java/com/utp/clsEstructuraDatos/pry3/Expression.java
+++ src/main/java/com/utp/clsEstructuraDatos/pry3/Expression.java
@@ -53,7 +53,7 @@ public sealed interface Expression {
         // Calculamos los posibles estados de la combinacion de variables
         int states = (int) Math.round(Math.pow(2, identificadores.size()));
         // fila_n es la fila del contenido
-        for (int fila_n = states - 1; fila_n >= 0; fila_n--) {
+        for (int fila_n = 0; fila_n < states; fila_n++) {
             // Quiero asegurarme que estoy trabajando con fila vacia.
             fila.clear();
 
@@ -62,8 +62,7 @@ public sealed interface Expression {
             // Invertimos el orden de los bits a LE
             // i.e. Si ultimos 3 bits de fila_n == 110, entonces estado_de_variables == 011
             for (int i = 0; i < identificadores.size(); i++) {
-                estado_de_variables = estado_de_variables << 1;
-                estado_de_variables = estado_de_variables | ((fila_n >> i) & 1);
+                estado_de_variables = (estado_de_variables << 1) | ((fila_n >> i) & 1);
             }
             for (Entry<String, Boolean> entry : lista_de_variables.entrySet()) {
                 entry.setValue(((estado_de_variables & 1) == 1));
@@ -361,11 +360,11 @@ public sealed interface Expression {
         @Override
         public boolean eval(HashMap<String, Boolean> boolean_map) {
             // if (op == Operator.IMPLICATES) {
-            //     System.out.println(boolean_map);
-            //     System.out.println(lhs + " " + op + " " + rhs + "_______");
-            //     System.out.println(lhs.eval(boolean_map) + " " + op + " "
-            //              + rhs.eval(boolean_map) + " = "
-            //              + op.eval(lhs.eval(boolean_map), Optional.of(rhs.eval(boolean_map))) + "\n");
+            // System.out.println(boolean_map);
+            // System.out.println(lhs + " " + op + " " + rhs + "_______");
+            // System.out.println(lhs.eval(boolean_map) + " " + op + " "
+            // + rhs.eval(boolean_map) + " = "
+            // + op.eval(lhs.eval(boolean_map), Optional.of(rhs.eval(boolean_map))) + "\n");
             // }
             return op.eval(lhs.eval(boolean_map), Optional.of(rhs.eval(boolean_map)));
         }
