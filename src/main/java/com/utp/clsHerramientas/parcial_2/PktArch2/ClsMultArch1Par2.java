7519dfa6f8787ff8e3af3a67735ef6c0f3dbfd94
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2024-05-21 21:39:37 -0500

    Comment out Parcial 2 content

--- src/main/java/com/utp/clsHerramientas/parcial_2/PktArch2/ClsMultArch1Par2.java
+++ src/main/java/com/utp/clsHerramientas/parcial_2/PktArch2/ClsMultArch1Par2.java
@@ -1,14 +1,14 @@
-package com.utp.clsHerramientas.parcial_2.PktArch2;
+// package com.utp.clsHerramientas.parcial_2.PktArch2;
 
-import java.math.BigInteger;
+// import java.math.BigDecimal;
 
-import com.utp.clsHerramientas.parcial_2.ThreeNumbers;
+// import com.utp.parcial_2.ThreeNumbers;
 
-class ClsMultArch1Par2 {
-    static BigInteger MtdMultPar2(ThreeNumbers threeNumbers) {
-        var num1 = threeNumbers.num1();
-        var num2 = threeNumbers.num2();
-        var num3 = threeNumbers.num3();
-        return num1.multiply(num2).multiply(num3);
-    }
-}
+// public class ClsMultArch1Par2 {
+//     public static BigDecimal MtdMultPar2(ThreeNumbers threeNumbers) {
+//         var num1 = threeNumbers.num1();
+//         var num2 = threeNumbers.num2();
+//         var num3 = threeNumbers.num3();
+//         return num1.multiply(num2).multiply(num3);
+//     }
+// }
--- src/main/java/com/utp/clsHerramientas/parcial_2/ThreeNumbers.java
+++
@@ -1,7 +0,0 @@
-package com.utp.clsHerramientas.parcial_2;
-
-import java.math.BigInteger;
-
-public record ThreeNumbers(BigInteger num1, BigInteger num2, BigInteger num3) {
-
-}
