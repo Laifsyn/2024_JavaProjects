5f66d28c2d8f4e6d0e1202a862529bd15154eb3f
Laifsyn <99366187+Laifsyn@users.noreply.github.com>
2024-05-30 20:59:35 +0000

    Definir implementaciones basicas

--- src/main/java/com/utp/clsEstructuraDiscretas/pry4/AppCombinatoria.java
+++ src/main/java/com/utp/clsEstructuraDiscretas/pry4/AppCombinatoria.java
@@ -1,12 +1,38 @@
 
 package com.utp.clsEstructuraDiscretas.pry4;
 
+import javax.swing.JFrame;
+import java.awt.GridBagLayout;
+
 public class AppCombinatoria {
-    
+
 }
 
 class UserInterface {
-    void spawn_app(){
-        
+    String pry4 = "Proyecto 4 - Combinatorias";
+    JFrame frame = new JFrame(pry4);
+
+    void run_app() {
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridBagLayout());
+
+        frame.setVisible(true);
+    }
+
+    public void SendCommand(Commands command) {
+        switch (command) {
+            case Commands.CalculateCombinatoria argumentos -> {
+            }
+            case Commands.UpdateRadios() -> {
+            }
+        }
     }
 }
+
+// @formatter:off
+sealed interface Commands {
+    public record CalculateCombinatoria(String n, String r) implements Commands {}
+    public record UpdateRadios() implements Commands {}
+}
+// @formatter:on
---
+++ src/main/java/com/utp/clsEstructuraDiscretas/pry4/Formulas.java
@@ -0,0 +1,33 @@
+package com.utp.clsEstructuraDiscretas.pry4;
+
+import java.math.BigInteger;
+
+import com.utp.utils.Result;
+
+public class Formulas {
+
+    public static Result<BigInteger, Exception> Comb_sin_repeticion(String[] args) {
+        return Result.error(new UnsupportedOperationException("Not supported yet."));
+    }
+
+    public static Result<BigInteger, Exception> Comb_con_repeticion(String[] args) {
+        return Result.error(new UnsupportedOperationException("Not supported yet."));
+    }
+
+    public static Result<BigInteger, Exception> Varianza_sin_repeticion(String[] args) {
+        return Result.error(new UnsupportedOperationException("Not supported yet."));
+    }
+
+    public static Result<BigInteger, Exception> Varianza_con_repeticion(String[] args) {
+        return Result.error(new UnsupportedOperationException("Not supported yet."));
+    }
+
+    public static Result<BigInteger, Exception> Permutacion_sin_repeticion(String[] args) {
+        return Result.error(new UnsupportedOperationException("Not supported yet."));
+    }
+
+    public static Result<BigInteger, Exception> Permutacion_con_repeticion(String[] args) {
+        return Result.error(new UnsupportedOperationException("Not supported yet."));
+    }
+
+}
