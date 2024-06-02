dc48b2863ba7c8b978a3fecb26d0bc0cab215031
Laifsyn <laifzing@gmail.com>
2024-06-02 16:34:32 -0500

    reordenar elementos

--- src/main/java/com/utp/clsEstructuraDiscretas/pry4/AppCombinatoria.java
+++ src/main/java/com/utp/clsEstructuraDiscretas/pry4/AppCombinatoria.java
@@ -35,18 +35,18 @@ import java.math.BigInteger;
  */
 public class AppCombinatoria {
     public static void main(String[] args) {
-        (new UserInterface()).run_app();
+        (new App()).run_app();
     }
 }
 
-class UserInterface {
+class App {
     String pry4 = "Proyecto 4 - Combinatorias";
     JFrame frame = new JFrame(pry4);
     final Entrada_JTextField entradas;
     final Preguntas preguntas;
     final JLabel resultado;
 
-    public UserInterface() {
+    public App() {
         this.entradas = new Entrada_JTextField(10);
         this.preguntas = new Preguntas(new SiNoRadioButton(), new SiNoRadioButton(), new SiNoRadioButton());
         this.resultado = new JLabel("Resultado:");
@@ -225,69 +225,6 @@ class Preguntas {
 
     }
 }
-
-enum VarianteFormula {
-    PERMUTACIONES, COMBINACIONES, VARIANZA, PERMUTACIONES_REPETIDAS, COMBINACIONES_REPETIDAS, VARIANZA_REPETIDAS;
-
-    static final Color Azul = new Color(1, 75, 160);
-    static final Color Morado = new Color(163, 73, 164);
-    static final Color Dorado = new Color(239, 184, 16);
-    static final Color Chocolate = new Color(105, 63, 38);
-    static final Color AzulClaro = new Color(0, 171, 240);
-    static final Color Magenta = new Color(200, 0, 167);
-
-    public Color as_color() {
-        switch (this) {
-            case PERMUTACIONES -> {
-                return Azul;
-            }
-            case COMBINACIONES -> {
-                return Morado;
-            }
-            case VARIANZA -> {
-                return Dorado;
-            }
-            case PERMUTACIONES_REPETIDAS -> {
-                return Chocolate;
-            }
-            case COMBINACIONES_REPETIDAS -> {
-                return AzulClaro;
-            }
-            case VARIANZA_REPETIDAS -> {
-                return Magenta;
-            }
-            default -> {
-                return Color.BLACK;
-            }
-        }
-
-    }
-
-    public Result<BigInteger, Exception> calcular(String n, String r) {
-        switch (this) {
-            case PERMUTACIONES -> {
-                return Formulas.Permutacion_sin_repeticion(new String[] { n, r });
-            }
-            case COMBINACIONES -> {
-                return Formulas.Comb_sin_repeticion(new String[] { n, r });
-            }
-            case VARIANZA -> {
-                return Formulas.Varianza_sin_repeticion(new String[] { n, r });
-            }
-            case PERMUTACIONES_REPETIDAS -> {
-                return Formulas.Permutacion_con_repeticion(new String[] { n, r });
-            }
-            case COMBINACIONES_REPETIDAS -> {
-                return Formulas.Comb_con_repeticion(new String[] { n, r });
-            }
-            case VARIANZA_REPETIDAS -> {
-                return Formulas.Varianza_con_repeticion(new String[] { n, r });
-            }
-        }
-        return Result.error(new UnsupportedOperationException("No se ha implementado la formula"));
-    }
-}
-
 class SiNoRadioButton {
     public final JRadioButton si;
     public final JRadioButton no;
@@ -370,3 +307,67 @@ sealed interface Commands {
     public record RepaintAll() implements Commands {}
 }
 // @formatter:on
+
+
+enum VarianteFormula {
+    PERMUTACIONES, COMBINACIONES, VARIANZA, PERMUTACIONES_REPETIDAS, COMBINACIONES_REPETIDAS, VARIANZA_REPETIDAS;
+
+    static final Color Azul = new Color(1, 75, 160);
+    static final Color Morado = new Color(163, 73, 164);
+    static final Color Dorado = new Color(239, 184, 16);
+    static final Color Chocolate = new Color(105, 63, 38);
+    static final Color AzulClaro = new Color(0, 171, 240);
+    static final Color Magenta = new Color(200, 0, 167);
+
+    public Color as_color() {
+        switch (this) {
+            case PERMUTACIONES -> {
+                return Azul;
+            }
+            case COMBINACIONES -> {
+                return Morado;
+            }
+            case VARIANZA -> {
+                return Dorado;
+            }
+            case PERMUTACIONES_REPETIDAS -> {
+                return Chocolate;
+            }
+            case COMBINACIONES_REPETIDAS -> {
+                return AzulClaro;
+            }
+            case VARIANZA_REPETIDAS -> {
+                return Magenta;
+            }
+            default -> {
+                return Color.BLACK;
+            }
+        }
+
+    }
+
+    public Result<BigInteger, Exception> calcular(String n, String r) {
+        switch (this) {
+            case PERMUTACIONES -> {
+                return Formulas.Permutacion_sin_repeticion(new String[] { n, r });
+            }
+            case COMBINACIONES -> {
+                return Formulas.Comb_sin_repeticion(new String[] { n, r });
+            }
+            case VARIANZA -> {
+                return Formulas.Varianza_sin_repeticion(new String[] { n, r });
+            }
+            case PERMUTACIONES_REPETIDAS -> {
+                return Formulas.Permutacion_con_repeticion(new String[] { n, r });
+            }
+            case COMBINACIONES_REPETIDAS -> {
+                return Formulas.Comb_con_repeticion(new String[] { n, r });
+            }
+            case VARIANZA_REPETIDAS -> {
+                return Formulas.Varianza_con_repeticion(new String[] { n, r });
+            }
+        }
+        return Result.error(new UnsupportedOperationException("No se ha implementado la formula"));
+    }
+}
+
