260a6823ced94cab1f1b12d0192d8f31b8808cb5
Philasarts <165176234+PhilsC8@users.noreply.github.com>
2024-06-05 20:07:21 +0000

    second commit

--- src/main/java/com/utp/clsEstructuraDiscretas/pry4/Formulas.java
+++ src/main/java/com/utp/clsEstructuraDiscretas/pry4/Formulas.java
@@ -8,7 +8,7 @@ public class Formulas {
 
     public static Result<BigInteger, Exception> Comb_sin_repeticion(String[] args) {
         var this_function = new Object() {}.getClass().getEnclosingMethod().getName();
-        return Result.error(new UnsupportedOperationException(this_function + " Not supported yet."));
+        return Result.error(new UnsupportedOperationException(this_function + " aun no esta listo."));
     }
 
     public static Result<BigInteger, Exception> Comb_con_repeticion(String[] args) {
